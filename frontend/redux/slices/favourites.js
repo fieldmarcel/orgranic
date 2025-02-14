46a0117831291a087b91c305b8a02b1b4b3b0bb6
fieldmarcel <shiva9590t@gmail.com>
2025-02-14 23:51:00 +0530

    y

---
+++ frontend/redux/slices/favourites.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    favourites: ,
+};
+
+
+export const favouritesSlice = createSlice({
+    name: "favourites",
+    initialState: {
+        favourites: [],
+   } })
