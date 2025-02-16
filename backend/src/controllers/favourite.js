219a571e476907448f4586b74a2f0dabc67a3f29
fieldmarcel <shiva9590t@gmail.com>
2025-02-16 07:33:05 +0530

    fav

--- backend/src/controllers/favourite.js
+++ backend/src/controllers/favourite.js
@@ -1,1 +1,12 @@
+import { Favourite } from '../models';
 
+const favourites= async (req, res) => {
+
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ success: false, message: "Internal server error" });
+        
+    }
+}
